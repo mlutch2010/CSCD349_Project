66a4ea515d20b1282906e69b82772c6402ce5da8
Zachary Burrows <zburrows7@gmail.com>
2015-05-07 09:48:30 -0700

    WarriorSlash now checks if attack is successful before performing it. UNTESTED.

--- AbilitiesProject_rev000/AbilitiesProject_rev000/WarriorSlash.cs
+++ AbilitiesProject_rev000/AbilitiesProject_rev000/WarriorSlash.cs
@@ -8,12 +8,14 @@ namespace CSCD349Project
 {
 	class WarriorSlash : IAttack
     {
+        private string abilityName;
         private Double _baseDamage;
         private Double _successRate;
         private Double _energyRequired;
 
         public WarriorSlash()
         {
+            this.abilityName = "Slash";
             this._baseDamage = 5.0;//5hp
             this._successRate = 0.90;//90%
             this._energyRequired = 5.0;//5 energy points (ep)
@@ -26,10 +28,18 @@ namespace CSCD349Project
             
             if(attackerAttributes._energy >= this._energyRequired)
             {
-                attackerAttributes._energy -= this._energyRequired;
+                if(attackSuccessful(this._successRate))
+                {
+                    attackerAttributes._energy -= this._energyRequired;
 
-                double healthLost = attackerAttributes._power * this._baseDamage - defenderAttributes._armor;
-                defenderAttributes._health -= healthLost;
+                    double healthLost = attackerAttributes._power * this._baseDamage - defenderAttributes._armor;
+                    defenderAttributes._health -= healthLost;
+                }
+                else
+                {
+                    string msg = "Attack Failed\n";// = attacker.getName() "\'s " + this.abilityName + " missed " + defender.getName() + "!";
+                    throw new AttackUnsuccesfulException(msg);
+                }
             }
             else
             {
@@ -38,7 +48,7 @@ namespace CSCD349Project
             }
         }
 
-        public bool attackSuccessful(Double attackSuccessRate)
+        private bool attackSuccessful(Double attackSuccessRate)
         {
             Random rnd = new Random();
             int percentChance = rnd.Next(0, 101);//generate random number between 0 and 100
@@ -48,7 +58,7 @@ namespace CSCD349Project
             return false;
         }
 
-        public bool defenseSuccessful(Double defenseSuccessRate)
+        private bool defenseSuccessful(Double defenseSuccessRate)
         {
             Random rnd = new Random();
             int percentChance = rnd.Next(0, 101);//generate random number between 0 and 100
