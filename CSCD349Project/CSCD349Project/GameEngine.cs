f5eecbecfd807a709bd41b8c8f87ac224c46e300
mikebean233 <michael@Michaels-MacBook-Pro.local>
2015-05-22 11:28:33 -0700

    Started making the game engine and working on images a and a level file format for the map

---
+++ CSCD349Project/CSCD349Project/GameEngine.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace CSCD349Project
+{
+
+    class GameEngine
+    {
+        private int noLevels = 1;
+        private List<Map> maps;
+
+        public static void Main(string[]args)
+        {
+            GameEngine thisGame = new GameEngine();
+            thisGame.Begin();
+        }
+
+        public GameEngine()
+        {
+            maps = new List<Map>();
+            // create the maps
+            for (int i = 0; i < noLevels; ++i)
+                maps.Add(readMapFile("level" + i + ".map"));
+
+        }
+
+        public void  Begin()
+        {
+            
+        }
+
+        public Map readMapFile(string path)
+        {
+            Map thisMap = null;
+            Cell[,] cells;
+            int noRows = 0;
+            int noCols = 0;
+            
+            // read in a map from a map file
+            try
+            {
+                // get dimensions
+                
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            thisMap = new Map(new int[] { noRows, noCols }); 
+            return thisMap;
+        }
+    
+    
+    
+        public void showErrorMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+    
+    }
+}
