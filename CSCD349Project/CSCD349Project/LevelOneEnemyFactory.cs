1d9a60fd30fb61ee84ecd9e486875ff0cbadf002
iamaudrey <greataudrey@gmail.com>
2015-05-25 21:41:29 -0700

    Created LevelOneEnemyFactory.cs
    Added the first concrete enemy factory. Again, it only returns one type of enemy, but right now we only have one concrete enemy. I'll flesh it out once we add more.

---
+++ CSCD349Project/CSCD349Project/LevelOneEnemyFactory.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCD349Project
+{
+    class LevelOneEnemyFactory : AbstractEnemyFactory
+    {
+        public LevelOneEnemyFactory()
+        { }
+
+        override public GameCharacter GetRandomGameCharacter()
+        {
+            return new LandShark();
+        }
+    }
+}
