754885a30472e86525caa193a5d3de6f3c65bc60
iamaudrey <greataudrey@gmail.com>
2015-05-25 21:51:33 -0700

    added enemy Party, GenerateRandomEnemy method
    added the _Enemies attribute, to contain the enemy party, and the GenerateRandomEnemies method to fill the Party with a random number of enemies.

--- CSCD349Project/CSCD349Project/Cell.cs
+++ CSCD349Project/CSCD349Project/Cell.cs
@@ -5,6 +5,7 @@
         private Map _MyMap;
         private int[] _Coordinates;
         private bool _Traversable;
+        private Party _Enemies;
         /*
         private bool _IsStart;
         private bool _IsFinish;
@@ -27,6 +28,12 @@
             _MyMap = thisMap;
             _Coordinates = coordinates;
             _Traversable = true;
+            _Enemies = new Party("Enemies", this);
+        }
+        
+        public Party _enemies
+        {
+            get {return _Enemies;}
         }
 
         public bool _traversable
@@ -36,6 +43,45 @@
         public Map _myMap
         {
             get { return _MyMap; }
+        }
+        
+        //This method takes a random number from a generator and determines the number
+        //of enemies that will be in the cell. Currently the range is from 0-4. Then
+        //it uses the Map's specific GameEnemyFactory to fill the party with random enemies.
+        //(Sorry, I know it looks a little bloated :( )
+        public void GenerateEnemies()
+        {
+            Random rnd = new Random();
+            int choice = rnd.Next(12);
+            if(choice <= 3)
+            {
+                //Empty party, no added enemies.
+            }
+            else if (choice > 3 && choice <= 6)
+            {
+                _Enemies.AddCharacter(_MyMap.GetFactory().GetRandomGameCharacter());
+            }
+            else if (choice > 6  || choice <= 9)
+            {
+                for(int i = 0; i < 2; i++)
+                {
+                    _Enemies.AddCharacter(_MyMap.GetFactory().GetRandomGameCharacter());
+                }
+            }
+            else if (choice == 10)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    _Enemies.AddCharacter(_MyMap.GetFactory().GetRandomGameCharacter());
+                }
+            }
+            else
+            {
+                for(int i = 0; i < 4; i++)
+                {
+                    _Enemies.AddCharacter(_MyMap.GetFactory().GetRandomGameCharacter());
+                }
+            }
         }
 
         
