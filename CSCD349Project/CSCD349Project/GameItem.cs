65a1dff07bfef384889dd227ba3fdcfdd96a224f
iamaudrey <greataudrey@gmail.com>
2015-05-25 21:58:27 -0700

    Fleshed out GameItem
    made GameItem abstract, added a string _Description attribute, and an abstract Use method.

--- CSCD349Project/CSCD349Project/GameItem.cs
+++ CSCD349Project/CSCD349Project/GameItem.cs
@@ -1,9 +1,17 @@
 namespace CSCD349Project
 {
-    public class GameItem
+    public abstract class GameItem
     {
+        private string _Description;
+        public string _description
+        {
+            get { return _Description; }
+            set { _Description = value; }
+        }
         public GameItem()
         {
         }
+        //Use method uses the given item on the current character.
+        public abstract void Use(GameCharacter character);
     }
 }
