b15c72095bc0582e38d83d552b38f503d4782719
mikebean233 <michael@Michaels-MacBook-Pro.local>
2015-05-27 11:45:23 -0700

    Created a class for handling messages, or logs.  (such as "Warrior attacks LandShark using Slice attack")

---
+++ CSCD349Project/CSCD349Project/Messaging.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCD349Project
+{
+    class Messaging
+    {
+
+        private List<string> messages;   
+        private int lastMessage;
+        public Messaging()
+        {
+            messages = new List<string>();
+            lastMessage = 0;
+        }
+        public void addMessage(string newMessage)
+        {
+            if(newMessage != null)
+                messages.Add(newMessage);
+        }
+    
+        public string getLatestMessages()
+        {
+            string output = "";
+            for (; lastMessage < messages.Count; ++lastMessage)
+                output += messages[lastMessage] + "\n";
+            return output;
+        }
+    
+    
+    
+    }
+}
